2bcfd68ac6a754bf0f5f6625e3f9f6c8d59fa18c
Dhiru <dhiru@alljobguider.in>
2025-06-04 14:48:26 +0530

    create utility function for access and refresh token

--- src/modules/auth/utils.ts
+++ src/modules/auth/utils.ts
@@ -1,1 +1,16 @@
+import jwt from "jsonwebtoken";
+import config from "../../config";
+
 // generate access token and refresh token
+export function generateAuthToken(
+  userId: string,
+  username: string
+): { accessToken: string; refreshToken: string } {
+  const accessToken = jwt.sign({ id: userId, username: username }, config.accessTokenSecret, {
+    expiresIn: "15m",
+  });
+  const refreshToken = jwt.sign({ id: userId, username: username }, config.refreshTokenSecret, {
+    expiresIn: "7d",
+  });
+  return { accessToken, refreshToken };
+}
